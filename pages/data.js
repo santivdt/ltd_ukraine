c3945e3245e23c00953883f7d275e5dbe5e348c1
Santi van den Toorn <santi.vdt@gmail.com>
2022-03-02 10:14:08 +0100

    V 0.1 super simple

---
+++ pages/data.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    label: "Amsterdam",
+    email: "amsterdam@amsterdam.nl",
+  },
+  {
+    label: "Tilburg",
+    email: "tilburg@tilburg.nl",
+  },
+  {
+    label: "Colosseum",
+    email: "",
+  },
+  {
+    label: "Bennekom",
+    email: "",
+  },
+  {
+    label: "Hallo",
+    email: " lorem",
+  },
+]
